4394a5da845d724568f4a338bc5e787a47451f20
Edilberto Mendez <betouis@hotmail.com>
2020-08-13 18:06:07 -0500

    DDL y redireccion de la pagina

--- IntegracionEvertec/IntegracionEvertec.cs
+++ IntegracionEvertec/IntegracionEvertec.cs
@@ -64,7 +64,7 @@ namespace PediosEvertec.IntegracionEvertec
                         mobile = pedido.customer_mobile
                     },
                     expiration = (DateTime.Now.AddDays(4)).ToString("yyyy-MM-ddTHH:mm:sszzz"),
-                    returnUrl = "http://localhost:60756/",
+                    returnUrl = "http://localhost:60756/Edit/" + pedido.id.ToString(),
                     userAgent = "Mozilla/5.0 (X11; Linux x86_64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/52.0.2743.82 Safari/537.36",
                     ipAddress = "127.0.0.1"
                 };
